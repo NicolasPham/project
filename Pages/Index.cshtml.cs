1e2aff72a59509b763a35512931bd4f6af200e7d
NicolasPham <phd.ngoc163@gmail.com>
2023-02-17 21:19:38 -0500

    updated

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -6,6 +6,10 @@ namespace project.Pages;
 public class IndexModel : PageModel
 {
     public List<string> movieTitles = new List<string>();
+    public List<string> posterURLs = new List<string>();
+    public string path = "https://image.tmdb.org/t/p/w500";
+    public List<string> overview = new List<string>();
+    public List<string> movieIDs = new List<string>();
     public PosterSet posterSet2 = new PosterSet();
     public async Task OnGet()
     {
@@ -13,6 +17,10 @@ public class IndexModel : PageModel
         posterSet2 = Fetch.posterSet;
         foreach(Poster poster in posterSet2.results) {
             movieTitles.Add(poster.title);
+            posterURLs.Add(path + poster.poster_path);
+            overview.Add(poster.overview);
+            movieIDs.Add(poster.id.ToString());
+
         }
     } //OnGet();
 } //class
